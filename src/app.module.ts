b2262e2f946bc93dba9ca25839b315d44fc7ee53
isaiasarza <arza.isaias@gmail.com>
2022-08-07 12:20:36 -0300

    Se agregan modulos de usuarios y authenticación utilizando JWT con secret provisoriamente hardcoded.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductController } from './product/product.controller';
 import { ProductService } from './product/product.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, AuthModule, UserModule],
   controllers: [AppController, ProductController],
-  providers: [AppService, ProductService],
+  providers: [ProductService],
 })
 export class AppModule {}
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { Observable, of } from 'rxjs';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { JwtAuthGuard } from 'src/auth/jwt.auth.guard';
 import { IController } from 'src/interfaces/controller.interface';
 import { IProduct } from './product.interface';
 import { ProductService } from './product.service';
@@ -10,26 +11,31 @@ export class ProductController implements IController<IProduct>{
     constructor(private readonly producService: ProductService){}
     
     @Get()
+    @UseGuards(JwtAuthGuard)
     findAll(): Observable<IProduct[]>{
         return this.producService.findAll()
     }
 
     @Post()
+    @UseGuards(JwtAuthGuard)
     create(@Body() product: IProduct): Observable<IProduct> {
         return this.producService.create(product)
     }
 
     @Delete()
+    @UseGuards(JwtAuthGuard)
     delete(@Param() params): Observable<IProduct> {
         return this.producService.delete(params.id)
     }
 
     @Get(':id')
+    @UseGuards(JwtAuthGuard)
     findOne(@Param() params): Observable<IProduct> {
         return this.producService.findOne(params.id)
     }
 
     @Put()
+    @UseGuards(JwtAuthGuard)
     update(@Body() product: IProduct): Observable<IProduct> {
         return this.producService.update(product)
     }
